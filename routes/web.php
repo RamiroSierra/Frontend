98493669ae3cb702137bbf0ef44bd0fae09edec3
Ramiro Sierra <ramiromateosierranunez@gmail.com>
2025-07-04 17:40:28 -0300

    no habia subido las rutas

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TareaController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', [TareaController::class, 'listar'])->name('home');
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/registrar', function () {return view('auth.registrar', ['loggedIn' => false]);})->name('registrar.form');
+Route::post('/registrar', [AuthController::class, 'registrar'])->name('registrar');
