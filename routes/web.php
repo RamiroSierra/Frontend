2916e37ca614950016da6b83b7a6ca3ecabd1548
Ramiro Sierra <ramiromateosierranunez@gmail.com>
2025-07-04 17:25:39 -0300

    creamos ruta web de home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TareaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TareaController::class, 'listar'])->name('home');
