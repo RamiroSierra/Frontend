773192fb8dad41d2cdd112ef02197cf03a5ad348
Ramiro Sierra <ramiromateosierranunez@gmail.com>
2025-07-04 17:30:11 -0300

    controlador de user, cremaos __construct y login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->authApiUrl = env('AUTH_API_URL', 'http://localhost:8000');
+    }
+
+    public function login(Request $request)
+    {
+        $response = Http::asForm()->post($this->authApiUrl.'/oauth/token', [
+            'grant_type' => 'password',
+            'client_id' => env('OAUTH_CLIENT_ID'),
+            'client_secret' => env('OAUTH_CLIENT_SECRET'),
+            'username' => $request->email,
+            'password' => $request->password,
+            'scope' => '',
+        ]);
+
+        if ($response->successful()) {
+            $data = $response->json();
+            Session::put('access_token', $data['access_token']);
+            Session::put('refresh_token', $data['refresh_token']);
+            return redirect('/');
+        }
+
+        return back()->with('error', 'Credenciales incorrectas');
+    }
+}
