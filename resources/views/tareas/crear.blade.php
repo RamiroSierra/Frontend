537515410e635b47c1404d109c29a64ca5e6ca42
Ramiro Sierra <ramiromateosierranunez@gmail.com>
2025-07-04 19:15:30 -0300

    greamos la vista crear

---
+++ resources/views/tareas/crear.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Editar Tarea</title>
+</head>
+<body>
+    <form action="{{ route('logout') }}" method="POST">
+        @csrf
+        <button type="submit">Deslogear</button>
+    </form>
+    <a href="{{ route('home') }}">inicio</a>
+    <h1>Editar Tarea: {{ $tarea['titulo'] }}</h1>
+    <form method="POST" action="{{ route('tareas.actualizar', $tarea['id']) }}">
+        @csrf
+        @method('PUT')
+        <input type="text" name="titulo" value="{{ $tarea['titulo'] }}" required><br>
+        <input type="number" name="asignado_id" value="{{ $tarea['asignado_id'] }}" required><br>
+        <textarea name="cuerpo" required>{{ $tarea['cuerpo'] }}</textarea><br>
+        <input type="date" name="fecha_expiracion" value="{{ $tarea['fecha_expiracion'] }}" required><br>
+        <input type="text" name="categorias" value="{{ implode(',', $tarea['categorias']) }}"><br>
+        <button type="submit">Actualizar Tarea</button>
+    </form>
+</body>
+</html>
